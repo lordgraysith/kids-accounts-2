1946e1c2af0a93c39bdbaa9faa01cb95697ffa86
Michael Graybeal <michael.graybeal@octanner.com>
2020-02-29 13:21:22 -0700

    pre-stringified

--- src/lambda/kids.js
+++ src/lambda/kids.js
@@ -3,6 +3,6 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({toot:"toot"})
+    body: '[{"id":1,"name":"Karli","mainAccount":{"id":1,"kidId":1,"accountTypeId":1,"accountType":"Main","balance":100}},{"id":2,"name":"Jack","mainAccount":{"id":2,"kidId":2,"accountTypeId":1,"accountType":"Main","balance":100}}]'
   });
 };
