03350bdda2eca43fc8530406f4a183bcf6fc5fea
Michael Graybeal <michael.graybeal@octanner.com>
2020-02-29 13:30:21 -0700

    this worked locally

--- src/lambda/kids.js
+++ src/lambda/kids.js
@@ -1,8 +1,8 @@
-// const {getKids} = require('../server/kids-service')
+const {getKids} = require('../server/kids-service')
 
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: '[{"id":1,"name":"Karli","mainAccount":{"id":1,"kidId":1,"accountTypeId":1,"accountType":"Main","balance":100}},{"id":2,"name":"Jack","mainAccount":{"id":2,"kidId":2,"accountTypeId":1,"accountType":"Main","balance":100}}]'
+    body: JSON.stringify(getKids())
   });
 };
